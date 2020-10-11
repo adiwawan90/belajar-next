221da4ea9631d0d1c4b3d41d27ccfe7744cba453
adiwawan90 <adi.wawan90@gmail.com>
2020-10-11 09:42:25 +0700

    fetching data and migrate some to TS

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,11 +1,29 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import { Link } from 'next/link';
+import {useState, useEffect} from 'react';
 
-const Hay = () => {
+const Hay = ({ownersList, ctx}) => {
     const router = useRouter();
 
-    console.log(router.query);
+    console.log('ini ctx: ',ctx);
     const {vehicle, person} = router.query;
+
+    const [owners, setOwners] = useState(ownersList);
+
+    useEffect(() => {
+        const loadData = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const ownersList = await response.json();
+            setOwners(ownersList)
+        }
+        if (ownersList.length == 0){
+            loadData();
+        }
+    },[])
+
+    if (!owners) {
+        return <h2>Loading ...</h2>
+    }
     return (
         <>
             <h1>Hellloooooo...,</h1> <br/>
@@ -15,3 +33,28 @@ const Hay = () => {
 }
 
 export default Hay
+
+Hay.getInitialProps = async ctx => {
+    if(!ctx.req) {
+        return {ownersList: []}
+    }
+   const { query } = ctx;
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const ownersList = await response.json();
+
+    return {
+        // owners: [
+        //     {
+        //         username: 'adi',
+        //         name: 'adi setyawan'
+        //     },
+        //     {
+        //         username: 'wawan',
+        //         name: 'adi guerero'
+        //     },
+            
+        // ]
+        ownersList: ownersList,
+        ctx
+    }
+}
--- src/pages/details.js
+++ src/pages/details.js
@@ -1,19 +1,31 @@
+import {useState, useEffect} from 'react';
 import Link from 'next/link';
 
-const Details = () => {
-    const datas = [
-        {v: 'car', p: 'adi'},
-        {v: 'bus', p: 'wawan'},
-        {v: 'motor', p: 'rony'},
-    ]
+const Details = ({owners}) => {
+    // const datas = [
+    //     {v: 'car', p: 'adi'},
+    //     {v: 'bus', p: 'wawan'},
+    //     {v: 'motor', p: 'rony'},
+    // ]
 
+    // const [owners, setOwners] = useState([]);
+    // useEffect(() => {
+    //     const loadData = async () => {
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    //         const ownersList = await response.json();
+    //         setOwners(ownersList)
+    //     }
+
+    //     loadData();
+    // },[])
     return (
         <div>
+        
             {
-                datas.map((item)=> (
-                    <div>
-                        <Link as={`/${item.v}/${item.p}`} href='/[vehicle]/[person]'>
-                            <a>Route to {`${item.v}/${item.p}`}</a>
+                owners.map((owner)=> (
+                    <div key={owner.id}>
+                        <Link as={`/${owner.username}/${owner.name}`} href='/[vehicle]/[person]'>
+                            <a>Route to {`${owner.username}/${owner.name}`}</a>
                         </Link>
                     </div>
                 ))
@@ -23,3 +35,23 @@ const Details = () => {
 }
 
 export default Details ;
+
+Details.getInitialProps = async ctx => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const ownersList = await response.json();
+
+    return {
+        // owners: [
+        //     {
+        //         username: 'adi',
+        //         name: 'adi setyawan'
+        //     },
+        //     {
+        //         username: 'wawan',
+        //         name: 'adi guerero'
+        //     },
+            
+        // ]
+        owners: ownersList
+    }
+}
